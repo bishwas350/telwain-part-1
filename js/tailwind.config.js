183073fb765a6ef5ad253e52a94967bc41eba0e2
bishwas350 <crashmindboy10@gmail.com>
2024-09-28 15:19:20 +0600

    fast commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // color 
+        
+        colors: {
+            // headerColor : 'rgba(22, 12, 109, 0.5)',
+              hoverColor:'#6A55EA', 
+        },
+        
+        // color 
+        // container 
+        maxWidth: {
+            'headerContainer': '1410px',
+            'bannerContainer': '967px',
+          },
+        // container 
+
+        // font 
+        fontFamily: {
+            'open': ['Open Sans'],
+            'nunito':['Nunito'],
+            'popi':['Poppins'],
+        },
+        // font 
+        // background 
+        backgroundImage: {
+            'bannerBg': "url('images/Header.jpg')",
+          },
+        // background 
+      }
+    }
+  }
